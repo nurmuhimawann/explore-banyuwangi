0b9961e31e1922061d5031aa6c877e73da2e3f7a
Nur Muhammad Himawan <89693885+nurmuhimawann@users.noreply.github.com>
2024-02-12 00:42:57 +0700

    Add utils for scroll up

---
+++ assets/js/scroll-up.js
@@ -0,0 +1,6 @@
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
